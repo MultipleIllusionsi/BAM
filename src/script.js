14ffc7a199334cf492c1b2dc25a784062c858d4e
MultipleIllusionsi <seregaserega06@gmail.com>
2019-10-26 07:38:53 +0500

    added new styling hover feature, added throttling for event

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ window.onload = function() {
 };
 
 const main = document.querySelector("menu");
-const wordWrappers = main.querySelectorAll(".word-wrapper");
+const collectionWord = document.querySelector(".collection-word");
 
 function appInit() {
   addTransformToContent();
@@ -13,14 +13,39 @@ function appInit() {
 
 function addTransformToContent() {
   window.onmousemove = function(e) {
-    let X = e.clientX;
-    let Y = e.clientY;
-
+    const X = e.clientX;
+    const Y = e.clientY;
     main.style.transform = `translate(-${X / 30}px, -${Y / 30}px)`;
 
-    for (const wrapper of wordWrappers) {
-      wrapper.style.transform = `translate(-${50 - X / 100}%, -${50 -
-        Y / 100}%)`;
+    const { dataset, parentNode } = e.target.parentNode;
+
+    if (dataset.title || parentNode.dataset.title) {
+      const titleOfSection =
+        dataset.title || parentNode.dataset.title;
+      collectionWord.innerHTML = titleOfSection;
+    } else {
+      collectionWord.innerHTML = "";
+    }
+
+    collectionWord.style.transform = `translate(-${50 -
+      X / 200}%, -${50 - Y / 200}%)`;
+  }.throttle(30);
+}
+
+/////// throttle
+Function.prototype.throttle = function(milliseconds, context) {
+  const baseFunction = this;
+  const limit = milliseconds;
+  let lastEventTimestamp = null;
+
+  return function() {
+    const self = context || this;
+    const args = arguments;
+    const now = Date.now();
+
+    if (!lastEventTimestamp || now - lastEventTimestamp >= limit) {
+      lastEventTimestamp = now;
+      baseFunction.apply(self, args);
     }
   };
-}
+};
